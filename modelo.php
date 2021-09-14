d3a16b08911aea1a1bfb09aa5688f705f3aa31b2
Allan_PC <Allan.FL.15@live.com>
2021-09-13 20:09:13 -0500

    agregue el codigo para la funcionalidad de sesiones

--- modelo.php
+++ modelo.php
@@ -1,5 +1,41 @@
 <?php 
 
+$operacion = $_POST['operacion'];
 
+if($operacion == 'login') {
+
+  $empleado = $_POST[''];
+
+  try {
+    include_once 'includes/funciones/conexionBD.php';
+    $stmt = $con->prepare("SELECT idEmpleado FROM OPERARIOS WHERE idEmpleado = ?");
+    $stmt->bind_param('i', $empleado);
+    $stmt->execute();
+    $stmt->bind_result($id_empleado);
+    if ($stmt->affected_rows) {
+      $existe = $stmt->fetch();
+      if ($existe) {
+        session_start();
+        $_SESSION['usuario'] = $id_empleado;
+        $respuesta = array(
+          'respuesta' => 'exito',
+        );
+      } else {
+        $respuesta = array(
+            'respuesta' => 'no_existe',
+        );
+      }
+    }
+    $stmt->close();
+    $con->close();
+  } catch (Exception $e) {
+    $respuesta = array(
+      'error' => $e->getMessage(),
+    );
+  }
+
+  echo json_encode($respuesta);
+
+}
 
 ?>
