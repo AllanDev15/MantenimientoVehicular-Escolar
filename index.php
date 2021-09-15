dfd420fbd50f17456304c30c08648f7fb0346ef9
Allan_PC <Allan.FL.15@live.com>
2021-09-15 16:21:18 -0500

    movi los estilos a un archivo css

--- index.php
+++ index.php
@@ -6,15 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Estilos -->
+    <link rel="stylesheet" href="css/styles.css">
     <title>Mantenimiento</title>
-    <style>
-         body {
-      background-image: url(https://ssl.sitew.org/images/blog/articles/fonds/e11.jpg);
-      background-size: cover;
-      background-position: center;
-
-    }
-    </style>
   </head>
   <body>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
