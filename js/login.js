86ceaa6957382d7dbc7bdcae9aaede36a0ef709f
Allan_PC <Allan.FL.15@live.com>
2021-09-17 08:52:25 -0500

    movi la validacion de login del supervisor

--- js/login.js
+++ js/login.js
@@ -7,30 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#formLogin');
     const inputId = document.querySelector('#inputId');
 
-    if (inputId.value == '2506') {
-      swal('Login exitoso', 'Bienvenido Supervisor', 'success');
-      setTimeout(() => {
-        window.location.href = 'reporte.php';
-      }, 2000);
-    } else {
-      const data = new FormData(form);
+    const data = new FormData(form);
 
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'modelo.php', true);
-      xhr.onload = function () {
-        if (this.status === 200 && this.readyState === 4) {
-          const response = JSON.parse(xhr.responseText);
-          if (response.respuesta === 'exito') {
-            swal('Login exitoso', `Bienvenido ${response.nombre}`, 'success');
-            setTimeout(() => {
-              window.location.href = 'operario.php';
-            }, 2000);
-          } else {
-            swal('Algo salio mal', 'No existe ningun usuario con ese numero de empleado', 'error');
-          }
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'modelo.php', true);
+    xhr.onload = function () {
+      if (this.status === 200 && this.readyState === 4) {
+        const response = JSON.parse(xhr.responseText);
+        if (response.respuesta === 'exito') {
+          swal('Login exitoso', `Bienvenido ${response.nombre}`, 'success');
+          setTimeout(() => {
+            window.location.href = 'index.php';
+          }, 2000);
+        } else {
+          swal('Algo salio mal', 'No existe ningun usuario con ese numero de empleado', 'error');
         }
-      };
-      xhr.send(data);
-    }
+      }
+    };
+    xhr.send(data);
   });
 });
