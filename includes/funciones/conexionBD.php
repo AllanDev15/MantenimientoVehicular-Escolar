75d02e21081ade812e8907eb77aa219b3eedc3e7
Allan_PC <Allan.FL.15@live.com>
2021-09-17 14:22:30 -0500

    agregue la conexion a la bd en heroku

--- includes/funciones/conexionBD.php
+++ includes/funciones/conexionBD.php
@@ -1,6 +1,16 @@
-<?php 
-    $con = new mysqli('localhost', 'root', '', 'mantenimiento');
-    if($con->connect_error) {
-        echo $error -> $con->connect_error;
-    }
-?>
+<?php
+
+// mysql://bcdf0b90bee309:4adcb88f@us-cdbr-east-04.cleardb.com/heroku_e73e215699bf695?reconnect=true
+// usuario : bcdf0b90bee309
+// pass : 4adcb88f
+// host : us-cdbr-east-04.cleardb.com
+
+$host = 'us-cdbr-east-04.cleardb.com';
+$usuario = 'bcdf0b90bee309';
+$pass = '4adcb88f';
+$database = 'heroku_e73e215699bf695';
+
+$con = new mysqli($host, $usuario, $pass, $database);
+if ($con->connect_error) {
+  echo $error->$con->connect_error;
+}
