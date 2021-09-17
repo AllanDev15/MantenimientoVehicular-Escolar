37b28a4ed00fe011cc72089222fea952aec6003b
Allan_PC <Allan.FL.15@live.com>
2021-09-17 13:13:23 -0500

    agregue icono para mostrar las refacciones

--- reporte.php
+++ reporte.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>Reportes</title>
 
   <style>
@@ -159,8 +160,8 @@
           <div class="col-md text-center">
             <p><?= $servicio['fechaSalida'] ?></p>
           </div>
-          <div class="col-md text-right">
-            <a href="#refaccion<?= $servicio['idVehiculo'] ?>" data-toggle="collapse" aria-expanded="false">Refacciones</a>
+          <div class="col-md text-center h3">
+            <a class="text-muted" href="#refaccion<?= $servicio['idVehiculo'] ?>" data-toggle="collapse" aria-expanded="false"><i class="fas fa-tools"></i></a>
           </div>
         </div>
 
